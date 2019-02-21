ef9ef3d8b27d9dc09b0ba97373fe76e5f2ae4295
Simon Bachmann <simonbachmann@Simons-MacBook-Pro.local>
2019-02-21 01:31:29 +0100

    UI update

---
+++ README.md
@@ -0,0 +1,32 @@
+# timestamp-dapp
+
+##Installation
+###IPFS must be installed and running
+**IPFS** installation guide can be found [here](https://docs.ipfs.io/introduction/install/).
+
+Run daemon with the following command:
+
+```
+ipfs daemon
+```
+
+###Truffe smart contract compilation and deployment
+```
+truffle develop
+```
+Then compile the contract
+
+```
+compile
+```
+and 
+
+```
+migrate
+```
+or if you have already migrated before
+
+```
+migrate --reset
+```
+
--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,90 +1,79 @@
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
+import {PropTypes} from 'react';
+import {DropzoneArea} from 'material-ui-dropzone';
 
-const React = require('react')
-const ipfsClient = require('ipfs-http-client')
+
+const React = require('react');
+const ipfsClient = require('ipfs-http-client');
 
 
 // create a stream from a file, which enables uploads of big files without allocating memory twice
-const fileReaderPullStream = require('pull-file-reader')
+const fileReaderPullStream = require('pull-file-reader');
 
 class FileUpload extends React.Component {
-    constructor () {
-        super()
+    constructor(props) {
+        super(props);
         this.state = {
             added_file_hash: null,
-            file:null,
-            fileUploaded:false
-        }
-        this.ipfs = ipfsClient('localhost', '5001')
+            file: null,
+        };
+        this.ipfs = ipfsClient('localhost', '5001');
 
         // bind methods
-        this.captureFile = this.captureFile.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.uploadFile = this.uploadFile.bind(this)
+        this.uploadFile = this.uploadFile.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    captureFile (event) {
-        event.stopPropagation()
-        event.preventDefault()
-        this.setState({file:event.target.files[0]})
+    handleChange(files) {
+        this.setState({
+            files: files,
+            file: files[0]
+        }, this.uploadFile);
+        this.props.updateFileSelected(true);
     }
 
-    uploadFile (event){
-        this.saveToIpfsWithFilename(this.state.file)
-        this.setState({fileUploaded:true})
+    uploadFile() {
+        this.saveToIpfsWithFilename(this.state.file);
+        this.props.updateFileUploaded(true);
     }
 
     // Add file to IPFS and wrap it in a directory to keep the original filename
-    saveToIpfsWithFilename (file) {
-        let ipfsId
-        const fileStream = fileReaderPullStream(file)
+    saveToIpfsWithFilename(file) {
+        let ipfsId;
+        const fileStream = fileReaderPullStream(file);
         const fileDetails = {
             path: file.name,
             content: fileStream
-        }
+        };
         const options = {
             wrapWithDirectory: true,
             progress: (prog) => console.log(`received: ${prog}`)
-        }
+        };
         this.ipfs.add(fileDetails, options)
             .then((response) => {
-                console.log(response)
+                console.log(response);
                 // CID of wrapping directory is returned last
-                ipfsId = response[response.length - 1].hash
-                console.log(ipfsId)
-                this.setState({added_file_hash: ipfsId})
-                this.props.setHash(ipfsId)
+                ipfsId = response[response.length - 1].hash;
+                console.log(ipfsId);
+                this.setState({added_file_hash: ipfsId});
+                this.props.setHash(ipfsId);
             }).catch((err) => {
             console.error(err)
         })
     }
 
-    handleSubmit (event) {
-        event.preventDefault()
-    }
-
-    render () {
+    render() {
         return (
             <div>
-                <form id='captureMedia' onSubmit={this.handleSubmit}>
-                    <br/>
-                    <input type='file' onChange={this.captureFile} />
-                    <br/>
-                    <br/>
-                    <Button variant="contained" color="primary" onClick={this.uploadFile}>
-                        Upload File to IPFS
-                    </Button>
-                </form>
                 <br/>
-                    {this.state.fileUploaded &&
-                    <Link href={'https://ipfs.io/ipfs/' + this.state.added_file_hash}>
-                        Find Your Uploaded File Here On IPFS
-                    </Link>
-                    }
-                <br/>
+                <DropzoneArea
+                    onChange={this.handleChange}
+                    filesLimit={1}
+                />
             </div>
         )
     }
 }
+
+
 export default FileUpload
